d6c2ce96425ba74278e7396ad58b75e33145a22a
Timur <timurgaliev27@yandex.ru>
2020-12-07 23:11:39 +0300

    Bug fixed

--- test.cpp
+++ test.cpp
@@ -5,14 +5,14 @@
 #include <string>
 using namespace std;
 
-template <typename C, typename S>
-S SUM(S& one, C& two){
+template <typename S>
+S SUM(S& one, S& two){
     return one + two;
 }
 
 
 int main(){
-   char n = 'Timur';
+   string n = 'Timur';
    string s = "Galiev";
    cout << SUM(s,n);
    return 0;
